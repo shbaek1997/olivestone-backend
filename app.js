f7386688b36aeeb06671f4f7940cf08ac7c380e7
shbaek1997 <shbaek1997@olivestonelab.com>
2022-11-01 02:46:08 -0700

    Feat: passport setting and user router login middleware

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var createError = require("http-errors");
 var express = require("express");
+const passport = require("passport");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+require("dotenv").config();
+const {
+  passportConfiguration,
+  JWTConfiguration,
+} = require("./service/auth.service");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const uploadRouter = require("./routes/upload-router");
@@ -20,7 +25,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(passport.initialize());
+passportConfiguration();
+JWTConfiguration();
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/upload", uploadRouter);
@@ -38,7 +45,9 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json({
+    error: err.message,
+  });
 });
 
 module.exports = app;
