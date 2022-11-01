0090a233f4dc3aecca842f4d4ae646bb04b461fd
shbaek1997 <shbaek1997@olivestonelab.com>
2022-10-31 22:00:53 -0700

    Feat: add mongoose,dotenv and set up mongo db

---
+++ db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const DB_URL = process.env.MONGO_DB_URL;
+console.log(DB_URL);
+
+module.exports = () => {
+  function connectDB() {
+    mongoose.connect(DB_URL, function (err) {
+      if (err) {
+        console.error("Mongo DB Connection Error", err);
+      }
+      console.log("Connected to MongoDB");
+    });
+  }
+  connectDB();
+  mongoose.connection.on("disconnected", connectDB);
+};
