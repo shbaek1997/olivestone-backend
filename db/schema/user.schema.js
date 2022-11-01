6c99ab0e9e0cc10f1bcaa9858b02503aa3cda903
shbaek1997 <shbaek1997@olivestonelab.com>
2022-10-31 22:55:53 -0700

    Feat:add user schema

---
+++ db/schema/user.schema.js
@@ -0,0 +1,24 @@
+const mongoose = reuqire("mongoose");
+const { Schema } = mongoose;
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: false,
+      default: "basic-user",
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+export { UserSchema };
