ea28fda27af0121362b8412f5c035471fe9ced09
shbaek1997 <shbaek1997@olivestonelab.com>
2022-11-01 21:13:58 -0700

    Feat: file meta data saved to db

--- routes/upload-router.js
+++ routes/upload-router.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const uploadRouter = express.Router();
 const upload = require("../middleware/upload");
+const { fileService } = require("../service/file.service");
 //post 요청의 key값이 single() 안의 문자열이어여 하고, 이 때 DB에 저장해야함..
-uploadRouter.post("/", upload.single("file"), (req, res, next) => {
+uploadRouter.post("/", upload.single("file"), async (req, res, next) => {
   const { file } = req;
-  console.log("file", file);
-  res.send("trying uploads");
+  const { password } = req.body;
+  const { originalname, mimetype, path, filename } = file;
+  const fileInfo = { originalname, password, mimetype, filename, path };
+  const savedFile = await fileService.saveFile(fileInfo);
+
+  res.json({
+    file: savedFile,
+  });
 });
 module.exports = uploadRouter;
