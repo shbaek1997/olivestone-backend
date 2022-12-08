6f7e56b0f4ca8e6fd36c687336bf2c8f98fa207a
shbaek1997 <shbaek1997@olivestonelab.com>
2022-12-08 17:42:37 +0900

    Feat:add user sign up api

--- routes/users-router.js
+++ routes/users-router.js
@@ -6,8 +6,12 @@ const passport = require("passport");
 const jwt = require("jsonwebtoken");
 // check login middleware
 const { loginRequired } = require("../middleware/auth-jwt");
+const { adminRequired } = require("../middleware/admin-required");
 //user service
 const { userService } = require("../service/user.service");
+const {
+  userRegisterJoiSchema,
+} = require("../db/schema/joi-schema/user.joi.schema");
 
 // get request with passport middleware to check if user is logged in
 userRouter.get("/auth", loginRequired, (req, res, next) => {
@@ -17,17 +21,44 @@ userRouter.get("/auth", loginRequired, (req, res, next) => {
 
 //user register post request to create new user (only for first user)
 //not used unless register page is made
-userRouter.post("/register", async (req, res, next) => {
-  try {
-    //body contains all the user info required
-    const { body } = req;
-    //create new user
-    const newUser = await userService.createNewUser({ ...body });
-    res.json({ user: newUser });
-  } catch (error) {
-    next(error);
+userRouter.post(
+  "/register",
+  loginRequired,
+  adminRequired,
+  async (req, res, next) => {
+    try {
+      //body contains all the user info required
+      //body should have password, repeat password, username
+      //check for repeating username..
+      //check validity of password, password repeat
+      //then create new user
+      const { body } = req;
+      const { email, fullname, password, passwordRepeat } = body;
+      const existingUser = await userService.getUserByEmail(email);
+      if (existingUser) {
+        throw new Error(
+          "해당 이메일은 이미 사용중입니다. 다른 이메일을 사용해주세요"
+        );
+      }
+      await userRegisterJoiSchema.validateAsync({
+        email,
+        fullname,
+        password,
+        passwordRepeat,
+      });
+      //create new user as basic-user
+      const newUser = await userService.createNewUser({
+        email,
+        fullname,
+        password,
+        passwordRepeat,
+      });
+      res.json({ user: newUser });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 // user login post request using passport.authenticate with local strategy
 userRouter.post("/login", async (req, res, next) => {
@@ -38,6 +69,8 @@ userRouter.post("/login", async (req, res, next) => {
       if (error || !user) {
         // passport fail or user = false
         // error message is in info variable
+        console.log(error);
+        console.log(info);
         const { message } = info;
         // respond error
         res.status(400).json({
@@ -56,13 +89,18 @@ userRouter.post("/login", async (req, res, next) => {
         //if everything was successful
         // sign jwt key using secret key
         const secretKey = process.env.JWT_SECRET_KEY; // login 성공시 key값을 써서 토큰 생성
-        const token = jwt.sign({ userId: user._id }, secretKey, {
-          expiresIn: "7d",
-        });
+        const token = jwt.sign(
+          { userId: user._id, role: user.role },
+          secretKey,
+          {
+            expiresIn: "7d",
+          }
+        );
         // respond with token and user id
         res.status(200).json({
           token,
           userId: user._id,
+          role: user.role,
         });
       });
     })(req, res);
