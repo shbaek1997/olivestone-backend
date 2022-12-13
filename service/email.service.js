3d16718c1b2bdf130889c4af569b4083111a0ffd
shbaek1997 <shbaek1997@olivestonelab.com>
2022-12-13 11:24:22 +0900

    Feat: add send validation mail function

---
+++ service/email.service.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+
+const sendValidationMail = async (receiverMailAddress) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL_ID,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  });
+  const email = receiverMailAddress;
+  const expireDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+  const info = { email, expireDate };
+  const token = jwt.sign(info, process.env.JWT_SECRET_KEY);
+  const mailOptions = {
+    from: `"Upload Download OliveStone" <${process.env.MAIL_ID}>`,
+    to: receiverMailAddress,
+    subject: "Verification Email",
+    html: `<p>Please click the link below to verify your email address:</p>
+    <a href="${process.env.HOME_PAGE}/users/verify?token=${token}">${process.env.HOME_PAGE}/users/verify?token=${token}</a>`,
+  };
+  const sentMailInfo = await transporter.sendMail(mailOptions);
+  console.log("mail sent", sentMailInfo);
+};
+
+module.exports = sendValidationMail;
