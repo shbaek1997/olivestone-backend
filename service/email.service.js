773bc3df25c56c942d178c1baaef34389bcd77f4
shbaek1997 <shbaek1997@olivestonelab.com>
2022-12-14 14:06:37 +0900

    Feat: change send validation mail function to EmailService class, add send password reset email method

--- service/email.service.js
+++ service/email.service.js
@@ -2,28 +2,43 @@ const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 
-const sendValidationMail = async (receiverMailAddress) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.MAIL_ID,
-      pass: process.env.MAIL_PASSWORD,
-    },
-  });
-  const email = receiverMailAddress;
-  // const expireDate = new Date(new Date().getTime() + 1000 * 10);
-  const expireDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-  const info = { email, expireDate };
-  const token = jwt.sign(info, process.env.JWT_SECRET_KEY);
-  const mailOptions = {
-    from: `"Upload Download OliveStone" <${process.env.MAIL_ID}>`,
-    to: receiverMailAddress,
-    subject: "Verification Email",
-    html: `<p>Please click the link below to verify your email address:</p>
-    <a href="${process.env.HOME_PAGE}/verify?token=${token}">${process.env.HOME_PAGE}/verify?token=${token}</a>`,
-  };
-  const sentMailInfo = await transporter.sendMail(mailOptions);
-  console.log("mail sent", sentMailInfo);
-};
-
-module.exports = sendValidationMail;
+class EmailService {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.MAIL_ID,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+  }
+  async sendValidationMail(email) {
+    // const expireDate = new Date(new Date().getTime() + 1000 * 10);
+    const expireDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+    const info = { email, expireDate };
+    const token = jwt.sign(info, process.env.JWT_SECRET_KEY);
+    const mailOptions = {
+      from: `"Upload Download OliveStone" <${process.env.MAIL_ID}>`,
+      to: email,
+      subject: "Verification Email",
+      html: `<p>Please click the link below to verify your email address:</p>
+      <a href="${process.env.HOME_PAGE}/verify?token=${token}">${process.env.HOME_PAGE}/verify?token=${token}</a>`,
+    };
+    const sentMailInfo = await this.transporter.sendMail(mailOptions);
+    console.log("mail sent", sentMailInfo);
+  }
+  //token 안의 내용은 user service로 확인..
+  async sendPasswordResetEmail(email, passwordResetToken) {
+    const mailOptions = {
+      from: `"Upload Download OliveStone" <${process.env.MAIL_ID}>`,
+      to: email,
+      subject: "Password Reset",
+      html: `<p>Follow this link below to reset your password</p>
+      <a href="${process.env.HOME_PAGE}/reset-password?token=${passwordResetToken}">${process.env.HOME_PAGE}/reset-password?token=${passwordResetToken}</a>`,
+    };
+    const sentMailInfo = await this.transporter.sendMail(mailOptions);
+    console.log("mail sent", sentMailInfo);
+  }
+}
+const emailService = new EmailService();
+module.exports = { emailService };
